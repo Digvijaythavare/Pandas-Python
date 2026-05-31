1d40395f400bd6d84c78a6824fecfc0d9506ffd1
Digvijay Thavare <digvijaythavare31@gmail.com>
2026-05-31 14:58:16 +0530

    Pandas Dataframe

---
+++ Dataframe.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+Student_name = ["John","Bob","Eve","Alice","Charlie"]
+Student_age = [20,23,22,29,21]
+Student_grade = [85,90,78,92,88]
+Student_city = ["New York","Los Angeles", "Chicago","Houston","Phoenis"]
+
+Student_info = pd.DataFrame({
+    "Name" : Student_name,
+    "Age" : Student_age,
+    "Grade" : Student_grade,
+    "City" : Student_city
+})
+
+print(Student_info)
