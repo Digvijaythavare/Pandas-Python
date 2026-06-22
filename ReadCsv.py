c528b037c469247e9bd324bc94720fe836d4622b
Digvijay Thavare <digvijaythavare31@gmail.com>
2026-06-22 07:39:17 +0530

    red csv file in python

--- ReadCsv.py
+++ ReadCsv.py
@@ -1,7 +1,23 @@
+# Head function returns the first 5 rows for the object basede on position.
+# Tail function is used to view last 5 rows of a Dataframe.
 import pandas as pd
 
 DataFrame = pd.read_csv('hello.csv')
 FirstColumn = 5
 lastColumn = 5
-print(DataFrame.head(FirstColumn))
+print(DataFrame.head(FirstColumn)) # 
 print(DataFrame.tail(lastColumn))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
